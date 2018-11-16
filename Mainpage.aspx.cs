51c5ae5d3eccc68c8c2f1c6914644e6c1e514ab0
NorahAbdul <45101184+NorahAbdul@users.noreply.github.com>
2018-11-16 18:16:08 +0300

    Add files via upload

---
+++ Mainpage.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Mainpage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label3.Text = Session["user"].ToString();
+        Label2.Visible = false;
+        Label4.Visible = false;
+        Label5.Visible = false;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Session["isadmin"].ToString() == "True")
+        {
+            Response.Redirect("adminpage.aspx");
+        }else
+        {
+            Label2.Visible = true;
+        }
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        if (Session["isDriver"].ToString() == "True")
+        {
+            Response.Redirect("Driverpage.aspx");
+        }
+        else
+        {
+            Label5.Visible = true;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        GridViewRow row = GridView1.SelectedRow;
+      
+        if (row!=null  )
+        {
+           SqlDataSource1.UpdateCommand = "UPDATE [golfCar] SET [Requested] = '1' WHERE [id] ='" + row.Cells[1].Text+ "'";
+           SqlDataSource1.Update();
+
+            SqlDataSource2.InsertCommand = "INSERT INTO[Orders] ([Requester],[CartNumber]) VALUES('"+ Session["user"].ToString().Trim() + "' , '"+ row.Cells[1].Text + "')";
+            SqlDataSource2.Insert();
+
+            Label4.Visible = true;
+        }
+        else
+        {
+            Label4.Text = "Sorry you cannot add any more because you either did not select or the Cart is full!!";
+            Label4.Visible = true;
+        }
+              
+    }
+}
---
+++ MasterPage.master.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        try
+        {
+            if (Session["user"].ToString().Length > 0)
+            {
+                Button1.Text = "Logout";
+                Label1.Text = Session["user"].ToString();
+            }
+            else
+            {
+                Button1.Text = "Login";
+                Label1.Visible = false;
+            }
+        }catch
+        {
+            Button1.Text = "Login";
+            Label1.Visible = false;
+        }
+       
+    }
+
+    protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Button1.Text == "Logout")
+        {
+            Session.RemoveAll();
+            Response.Redirect("Registration.aspx");
+        }
+        else
+        {
+            Response.Redirect("Registration.aspx");
+        }
+    }
+}
